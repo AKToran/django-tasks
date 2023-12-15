a292410af4aa11bbb85bd44a2f0f63ae40c05cae
AKToran <aktoran60@gmail.com>
2023-12-16 01:36:36 +0600

    login required implemented

--- Week 5 Practice 2/music_directory/musician/views.py
+++ Week 5 Practice 2/music_directory/musician/views.py
@@ -2,14 +2,17 @@ from . import forms
 from . import models
 from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
+@method_decorator(login_required, name='dispatch')
 class CreateMusicianView(CreateView):
     model = models.Musucian
     form_class = forms.MusucianForm
     success_url = reverse_lazy('home')
     template_name = 'add_musician.html'
 
-
+@method_decorator(login_required, name='dispatch')
 class EditMusicianView(UpdateView):
     model = models.Musucian
     form_class = forms.MusucianForm
